35300f9a4d2212af5ece2ca0b796055e958deab7
rsefraou <rita.sefraouitahiri@unil.ch>
2019-11-24 20:24:04 +0100

    modify descriptions

--- R/get_sunsentiments.R
+++ R/get_sunsentiments.R
@@ -1,20 +1,12 @@
-#' Get a tidy data frame of a single sentiment lexicon
+#' @title get_sunsentiments
 #'
-#' Get specific sentiment lexicons in a tidy format, with one row per word,
-#' in a form that can be joined with a one-word-per-row dataset.
-#' The \code{"bing"} option comes from the included \code{\link{sentiments}}
-#' data frame, and others call the relevant function in the \pkg{textdata}
-#' package.
-#'
-#' @param lexicon The sentiment lexicon to retrieve;
-#' either "afinn", "bing", "nrc", or "loughran" or "sunscReen"
-#'
-#' @return A tbl_df with a \code{word} column, and either a \code{sentiment}
-#' column (if \code{lexicon} is not "afinn") or a numeric \code{score} column
-#' (if \code{lexicon} is "afinn").
-#'
-#'
+#' @describeIn get_sunsentiments gets sentiments about sunscreens depending on words
+#' @param lexicon A \code{character} (character) used to name the lexicon
+#' @return A \code{dataframe} of words with sentiments
+#' @import magrittr
 #' @export
+#' @examples
+#' get_sunsentiments("sunscReen")
 get_sunsentiments <- function(lexicon = c("sunscReen")) {
   lexicon <- match.arg(lexicon)
 
@@ -22,24 +14,12 @@ get_sunsentiments <- function(lexicon = c("sunscReen")) {
 }
 
 
-#' @family lexicon
-#' @importFrom fs file_exists dir_exists dir_create path
-#' @importFrom readr read_rds
-#' @importFrom utils menu
+#' @title lexicon_sunscReen
+#'
+#' @describeIn lexicon_sunscReen is used in getsunsentiments
+#' @return A \code{dataframe} of sentiments
 #' @export
-#' @examples
-#' \donttest{
-#' lexicon_sunscReen()
-#'
-#' # Custom directory
-#' lexicon_sunscReen(dir = "data/")
-#'
-#' # Deleting dataset
-#' lexicon_sunscReen(delete = TRUE)
-#'
-#' # Returning filepath of data
-#' lexicon_sunscReen(return_path = TRUE)
-#' }
+#' @import magrittr dplyr
 lexicon_sunscReen <- function(dir = NULL, delete = FALSE, return_path = FALSE,
                               clean = FALSE) {
   load_dataset(data_name = "sunscReen", name = "words.rds",
